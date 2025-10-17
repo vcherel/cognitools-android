17e58ee5aa2e2b42fc8c46ab0f26f9b76718dd6c
Valentin CHEREL <valentin.cherel@insa-rennes.fr>
2025-10-17 23:41:08 +0200

    Changed fontweight of link

--- app/src/main/java/com/example/myapp/Wikipedia.kt
+++ app/src/main/java/com/example/myapp/Wikipedia.kt
@@ -30,12 +30,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -290,8 +288,7 @@ fun appendElementRecursively(element: org.jsoup.nodes.Element, builder: Annotate
             val text = element.text()
             builder.pushStringAnnotation(tag = "URL", annotation = url)
             builder.withStyle(style = SpanStyle(
-                color = Color.Blue,
-                textDecoration = TextDecoration.Underline
+                fontWeight = FontWeight.Bold
             )
             ) {
                 append(text)
